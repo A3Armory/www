0f825d81a344aa48a2d77ee3d66295a67f848c14
micovery <miguel@micovery.com>
2015-05-03 03:49:41 -0400

    Switch to ES default port

--- application/config/globals.php
+++ application/config/globals.php
@@ -5,21 +5,21 @@ $config["a3w_servers"] = array(
     "PlayersList_ID" => "A3W_PlayersList",
     "VehiclesList_ID" => "A3W_Vehicles",
     "ObjectsList_ID" => "A3W_Objects",
-    "ES_Players_Index" => "http://108.61.136.218/players",
-    "ES_PlayersList_Index" => "http://108.61.136.218/players_list",
-    "ES_PlayersList_History_Index" => "http://108.61.136.218/players_list_history",
-    "ES_VehiclesList_History_Index" => "http://108.61.136.218/vehicles_list_history",
-    "ES_ObjectsList_History_Index" => "http://108.61.136.218/objects_list_history"
+    "ES_Players_Index" => "http://108.61.136.218:9200/players",
+    "ES_PlayersList_Index" => "http://108.61.136.218:9200/players_list",
+    "ES_PlayersList_History_Index" => "http://108.61.136.218:9200/players_list_history",
+    "ES_VehiclesList_History_Index" => "http://108.61.136.218:9200/vehicles_list_history",
+    "ES_ObjectsList_History_Index" => "http://108.61.136.218:9200/objects_list_history"
   ),
   "stratis" => array(
     "PlayersList_ID" => "A3W2_PlayersList",
     "VehiclesList_ID" => "A3W2_Vehicles",
     "ObjectsList_ID" => "A3W2_Objects",
-    "ES_Players_Index" => "http://108.61.136.218/players",
-    "ES_PlayersList_Index" => "http://108.61.136.218/players_list",
-    "ES_PlayersList_History_Index" => "http://108.61.136.218/players_list_history",
-    "ES_VehiclesList_History_Index" => "http://108.61.136.218/vehicles_list_history",
-    "ES_ObjectsList_History_Index" => "http://108.61.136.218/objects_list_history"
+    "ES_Players_Index" => "http://108.61.136.218:9200/players",
+    "ES_PlayersList_Index" => "http://108.61.136.218:9200/players_list",
+    "ES_PlayersList_History_Index" => "http://108.61.136.218:9200/players_list_history",
+    "ES_VehiclesList_History_Index" => "http://108.61.136.218:9200/vehicles_list_history",
+    "ES_ObjectsList_History_Index" => "http://108.61.136.218:9200/objects_list_history"
   )
 );
 
